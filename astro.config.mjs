81bbf2d2074ed71edfd4b1ce0eb05615fffb445a
yeonjuan <yeonjuan93@naver.com>
2024-02-17 02:01:11 +0900

    fixup

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,14 +17,14 @@ const SERVER_PORT = 3000;
 // the url to access your blog during local development
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
 // the url to access your blog after deploying it somewhere (Eg. Netlify)
-const LIVE_URL = "https://yourwebsiteurl.com";
+const LIVE_URL = "https://yeonjuan.github.io";
 // this is the astro command your npm script runs
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
 let BASE_URL = LOCALHOST_URL;
 // When you're building your site in local or in CI, you could just set your URL manually
 if (isBuild) {
-  BASE_URL = LIVE_URL;
+  BASE_URL = "LIVE_URL";
 }
 
 export default defineConfig({
