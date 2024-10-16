0f5f6adf49ed1189b4c6377c771b29d0609d6f90
YeonJuan <yeonjuan93@naver.com>
2024-10-16 10:53:31 +0800

    renewal all

---
+++ src/content/config.ts
@@ -0,0 +1,13 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+	type: 'content',
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		heroImage: z.string().optional(),
+	}),
+});
+
+export const collections = { blog };
